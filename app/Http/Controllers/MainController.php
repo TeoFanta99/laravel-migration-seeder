3200a723d5a7ffb5b86b6186dcad639b98965809
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-13 16:33:17 +0100

    add controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class MainController extends Controller
+{
+    public function index() {
+
+        $trains = Train :: all();
+
+        return view('pages.index', compact('trains'));
+    }
+    
+}
