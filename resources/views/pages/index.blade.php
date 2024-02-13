70fe953475374b5164859453906e317d755bcdc8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-13 17:12:28 +0100

    stampa in pagina dati

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,15 @@
     <ul>
         @foreach ($trains as $train)
             <li>
-                {{$train -> stazione_di_partenza}}
+                {{$train -> codice_treno}} - 
+                {{$train -> azienda}} - 
+                {{$train -> stazione_di_partenza}} - 
+                {{$train -> stazione_di_arrivo}} - 
+                {{$train -> orario_di_partenza}} - 
+                {{$train -> orario_di_arrivo}} - 
+                {{$train -> numero_di_carrozze}} -
+                {{$train -> in_orario}} -
+                {{$train -> cancellato}}
             </li>
         @endforeach
     </ul>
