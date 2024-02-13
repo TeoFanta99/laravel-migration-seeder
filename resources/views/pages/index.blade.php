bd49647a8b659cbab396a9c92087ed07b47be0a8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-13 17:27:24 +0100

    edit foreach

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,18 +5,41 @@
 @section('content')
     <h1>Trains: </h1>
     <ul>
-        @foreach ($trains as $train)
-            <li>
-                {{$train -> codice_treno}} - 
-                {{$train -> azienda}} - 
-                {{$train -> stazione_di_partenza}} - 
-                {{$train -> stazione_di_arrivo}} - 
-                {{$train -> orario_di_partenza}} - 
-                {{$train -> orario_di_arrivo}} - 
-                {{$train -> numero_di_carrozze}} -
-                {{$train -> in_orario}} -
-                {{$train -> cancellato}}
-            </li>
-        @endforeach
+
     </ul>
+    <div class="container">
+        <div class="row">
+        @foreach ($trains as $train)
+            <div class="col-12 col-lg-3 col-md-4 col-sm-6 col">
+            Codice treno: {{$train -> codice_treno}}
+            <br>
+            <br>
+            Azienda: {{$train -> azienda}}
+            <br>
+            <br>
+            Staz. Partenza: {{$train -> stazione_di_partenza}}
+            <br>
+            <br>
+            Staz. Arrivo: {{$train -> stazione_di_arrivo}}
+            <br>
+            <br>
+            Orario Partenza: {{$train -> orario_di_partenza}}
+            <br>
+            <br>
+            Orario Arrivo: {{$train -> orario_di_arrivo}}
+            <br>
+            <br>
+            N. di carrozze: {{$train -> numero_di_carrozze}}
+            <br>
+            <br>
+            È in orario? {{$train -> in_orario}}
+            <br>
+            <br>
+            È cancellato? {{$train -> cancellato}}
+            <br>
+            <br>
+           </div>
+        @endforeach
+        </div>
+    </div>
 @endsection
